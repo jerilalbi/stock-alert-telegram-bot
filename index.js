af79b1a990fe63616b824b1f1b9ecb4e4128dbb2
Jeril Albi <devjerilalbi@gmail.com>
2024-06-14 14:43:50 +0530

    run task every minute success

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 const marubozuStocks = require('./marubozuStocks');
+const telegram = require('./telegram')
+const eventEmitter = require('./eventEmitter');
 
-(async()=>{
+let chatdID;
+
+async function getdata(){
     const marubozuStockData = await marubozuStocks();
     console.log(marubozuStockData.bullishStockData);
+}
+
+telegram.startMessage();
+
+console.log('Task scheduler is running...');
+
+function tasksheldule(){
+    eventEmitter.on("chatID",(id) =>{
+        chatdID = id;
+    })
+    if(typeof chatdID !== 'undefined'){
+        telegram.sendMessage({id: chatdID,message: new Date().getMinutes()});
+    }
+    console.log("Time = "+ new Date().getMinutes())
+}
+
+(function timeScheduler(){
+    const date = new Date();
+    const currentDay = date.getDay();
+    const currentHour = date.getHours();
+    const currentMinute = date.getMinutes(); 
+
+    const isWeekDay = currentDay>= 1 && currentDay <= 5;
+    const isWithInTime = (currentHour === 9 && currentMinute >= 15) || (currentHour > 9 && currentHour < 15) || (currentHour >= 15 && currentMinute <= 15);
+
+    if(isWeekDay && isWithInTime){
+        tasksheldule();
+    }
+
+    setTimeout(timeScheduler,60 * 1000)
 })()
+
--- telegram.js
+++ telegram.js
@@ -1,18 +1,35 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
+const eventEmitter = require('./eventEmitter');
 
 const bot = new TelegramBot(process.env.TOKEN,{polling: true})
+let chatId;
 
-bot.on("message", (msg) =>{
-    switch(msg.text){
-        case '/start':
-        sendMessage({id: msg.from.id,message: "Hello, \nI will send you marubozu stocks in each minute from 9:00 AM to 3PM in Monday to Friday"})
+function startMessage(){
+    try{
+        bot.on("message", (msg) =>{
+            switch(msg.text){
+                case '/start':
+                sendMessage({id: msg.from.id,message: "Hello, \nI will send you marubozu stocks in each minute from 9:00 AM to 3PM in Monday to Friday"});
+                break;
+                default: sendMessage({id: msg.from.id, message: "Dont't worry I will send you stock recommendation"})
+            }
+            chatId = msg.from.id;
+            eventEmitter.emit("chatID",chatId);
+        })
+    }catch(error){
+        console.error(error);
     }
-    console.log(msg)
-})
+    
+}
 
-sendMessage();
 
 function sendMessage({id,message}){
-    bot.sendMessage(id,message);
+    try{
+        bot.sendMessage(id,message);
+    }catch(error){
+        console.error(error);
+    }
 }
+
+module.exports = {sendMessage, startMessage}
